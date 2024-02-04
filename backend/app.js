e72e987271d55fcde4ff50d08c1580b5930aa1c4
Sandeepan Kalita <77795912+redhawk2002@users.noreply.github.com>
2024-02-04 21:15:17 +0530

    Update app.js
    added few comments

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const haversineDistance = require("./haversineDistance");
 // Parse incoming request bodies in a middleware before handlers
 app.use(bodyParser.json());
 app.use(cookieParser());
-
+// Root endpoint to welcome users
 app.get("/", async (req, res) => {
   try {
     res
@@ -26,6 +26,7 @@ app.get("/", async (req, res) => {
     });
   }
 });
+// Endpoint for user registration
 app.post("/api/register", async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -36,14 +37,14 @@ app.post("/api/register", async (req, res) => {
         .status(400)
         .json({ success: false, message: "please check the email id " });
     }
-
+ // Checking if the user already exists
     let user = await User.findOne({ email });
     if (user) {
       return res
         .status(400)
         .json({ success: false, message: "user already exist" });
     }
-
+ // Creating a new user
     user = await User.create({
       email,
       password,
@@ -60,11 +61,12 @@ app.post("/api/register", async (req, res) => {
     });
   }
 });
-
+// Endpoint for user login
 app.post("/api/login", async (req, res) => {
   try {
+    // Extracting email and password from the request body
     const { email, password } = req.body;
-
+ // Finding the user in the database
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({
@@ -72,20 +74,20 @@ app.post("/api/login", async (req, res) => {
         message: "User does not exist",
       });
     }
-
+// Checking if the password is correct
     const isMatch = password == user.password;
     if (!isMatch) {
       return res
         .status(400)
         .json({ success: false, message: "Incorrect password" });
     }
-
+ // Creating a JWT token for authentication
     const token = jwt.sign({ userEmail: email }, process.env.JWT_SECRET);
     const options = {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
       httpOnly: true,
     };
-
+ // Sending the token as a cookie
     res.status(201).cookie("token", token, options).json({
       success: true,
       user,
@@ -98,9 +100,10 @@ app.post("/api/login", async (req, res) => {
     });
   }
 });
-
+// Endpoint for user logout
 app.get("/api/logout", async (req, res) => {
   try {
+    // Logging out the user by clearing the token cookie
     res
       .status(200)
       .cookie("token", null, {
@@ -118,11 +121,13 @@ app.get("/api/logout", async (req, res) => {
     });
   }
 });
-
+// Endpoint for writing user location in db
 app.post("/api/location", isAuthenticated, async (req, res) => {
   try {
-    const { latitude, longitude } = req.body;
 
+    // Extracting latitude and longitude from the request body
+    const { latitude, longitude } = req.body;
+// Validating latitude and longitude values
     if (!latitude || !longitude) {
       return res.status(400).json({
         message: "Latitude and longitude are required",
@@ -140,12 +145,12 @@ app.post("/api/location", isAuthenticated, async (req, res) => {
         message: "Invalid longitude",
       });
     }
-
+// Creating a new coordinate entry
     const newCordinateEntry = {
       latitude: latitude,
       longitude: longitude,
     };
-
+ // Adding the new coordinate entry to the database
     await Cord.create(newCordinateEntry);
 
     res.status(200).json({
@@ -159,7 +164,7 @@ app.post("/api/location", isAuthenticated, async (req, res) => {
     });
   }
 });
-
+// Endpoint for calculating distance between two coordinates
 app.post("/api/distance", isAuthenticated, async (req, res) => {
   try {
     const { cord1, cord2 } = req.body;
@@ -168,17 +173,11 @@ app.post("/api/distance", isAuthenticated, async (req, res) => {
         message: "coordinate-1 and coordinate-2 are required",
       });
     }
-
+// Extracting latitude and longitude from each coordinate
     const { latitude: latitude1, longitude: longitude1 } = cord1;
     const { latitude: latitude2, longitude: longitude2 } = cord2;
 
-    const distance = haversineDistance(
-      latitude1,
-      longitude1,
-      latitude2,
-      longitude2
-    );
-
+// Validating latitude and longitude values
     if (latitude1 > 90.0 || latitude1 < -90.0) {
       return res.status(400).json({
         message: "Invalid latitude",
@@ -201,7 +200,13 @@ app.post("/api/distance", isAuthenticated, async (req, res) => {
         message: "Invalid longitude",
       });
     }
-
+ // Calculating haversine distance between coordinates
+    const distance = haversineDistance(
+      latitude1,
+      longitude1,
+      latitude2,
+      longitude2
+    );
     res.status(201).json({
       success: true,
       distance: distance,
@@ -214,10 +219,11 @@ app.post("/api/distance", isAuthenticated, async (req, res) => {
     });
   }
 });
+// Endpoint for finding the closest coordinate to a given point
 app.post("/api/closest", isAuthenticated, async (req, res) => {
   try {
     const { latitude, longitude } = req.body;
-
+// Validating latitude and longitude values
     if (!latitude || !longitude) {
       return res.status(400).json({
         message: "Latitude and longitude are required",
@@ -234,11 +240,11 @@ app.post("/api/closest", isAuthenticated, async (req, res) => {
         message: "Invalid latitude or longitude",
       });
     }
-
+// Retrieving all coordinates from the database
     const allCoordinates = await Cord.find();
     let closestCoordinate = null;
     let minDistance = Infinity;
-
+// Finding the closest coordinate using haversine distance
     for (coordinate of allCoordinates) {
       const distance = haversineDistance(
         latitude,
